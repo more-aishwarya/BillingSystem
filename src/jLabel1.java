15646b03942c84b83fea494ea5d0cb1f92919658
Aishwarya More <35859174+more-aishwarya@users.noreply.github.com>
2024-03-11 09:33:24 -0400

    Add files via upload

---
+++ src/jLabel1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mansi
+ */
+class jLabel1 {
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
